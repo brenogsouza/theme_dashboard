2435419c17a6f999791ea40b7fcda6c61d839a9e
brenogsouza <breno.souza.developer@gmail.com>
2020-02-20 00:24:08 -0300

    settings in bugfix

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,3 +4,4 @@ mongoose.connect(
   "mongodb+srv://breno:12345@meubanco-tgxc0.mongodb.net/dashtemplate?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+mongoose.Error.messages.general.required = 'O atributo "{PATH}" é obrigatório';
--- backend/src/models/Schemas.js
+++ backend/src/models/Schemas.js
@@ -7,9 +7,13 @@ const CreditSchema = new mongoose.Schema({
   value: { type: Number, min: 0, required: true }
 });
 
-const DebitSchema = new mongoose.Schema({
+const DebtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Informe o valor do débito"]
+  },
   status: {
     type: String,
     required: false,
@@ -20,10 +24,10 @@ const DebitSchema = new mongoose.Schema({
 
 const BillingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  month: { Type: Number, min: Number, max: Number },
-  year: { Type: Number, min: Date, max: Date },
+  month: { type: Number, min: 1, max: 12, required: true },
+  year: { type: Number, min: 1992, max: 2100, required: true },
   credits: [CreditSchema],
-  debts: [DebitSchema]
+  debts: [DebtSchema]
 });
 
 module.exports = restful.model("BillingCycle", BillingCycleSchema);
