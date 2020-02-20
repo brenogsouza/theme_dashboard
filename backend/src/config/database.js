a05b7a9a7be47fdfdc623e33e15668762d2a8473
brenogsouza <breno.souza.developer@gmail.com>
2020-02-20 00:39:51 -0300

    set messages error in DB

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,4 +4,14 @@ mongoose.connect(
   "mongodb+srv://breno:12345@meubanco-tgxc0.mongodb.net/dashtemplate?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
 mongoose.Error.messages.general.required = 'O atributo "{PATH}" é obrigatório';
+
+mongoose.Error.messages.Number.min =
+  'O "{VALUE}" informado é menor que o limite mínimo de "{MIN}"';
+
+mongoose.Error.messages.Number.max =
+  'O "{VALUE}" informado é maior que o limite máximo de "{MAX}"';
+
+mongoose.Error.messages.Number.max =
+  'O "{VALUE}" não é válido para o atributo "{PATH}"';
--- backend/src/models/Schemas.js
+++ backend/src/models/Schemas.js
@@ -4,7 +4,11 @@ const mongoose = restful.mongoose;
 
 const CreditSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true }
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Informe o valor do crédito!"]
+  }
 });
 
 const DebtSchema = new mongoose.Schema({
@@ -12,7 +16,7 @@ const DebtSchema = new mongoose.Schema({
   value: {
     type: Number,
     min: 0,
-    required: [true, "Informe o valor do débito"]
+    required: [true, "Informe o valor do débito!"]
   },
   status: {
     type: String,
@@ -24,8 +28,13 @@ const DebtSchema = new mongoose.Schema({
 
 const BillingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  month: { type: Number, min: 1, max: 12, required: true },
-  year: { type: Number, min: 1992, max: 2100, required: true },
+  month: { type: Number, min: 1, max: 12, required: [true, "Informe o mês!"] },
+  year: {
+    type: Number,
+    min: 1992,
+    max: 2100,
+    required: [true, "Informe o ano!"]
+  },
   credits: [CreditSchema],
   debts: [DebtSchema]
 });
