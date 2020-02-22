0ffa2b895d815d4bc8ccdafc53bc36709cc84c6b
brenogsouza <breno.souza.developer@gmail.com>
2020-02-21 23:16:13 -0300

    set middleware query-parser-int

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,10 +2,12 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
 const allowCors = require("./cors");
+const queryParser = require("express-query-int");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
+server.use(queryParser());
 
 const port = 3003;
 server.listen(port, () => {
